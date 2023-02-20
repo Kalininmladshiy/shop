7a9747e49d95d874a45cc77c2eb775297ebdd708
Maksim <106922768+Kalininmladshiy@users.noreply.github.com>
2023-02-20 11:22:51 +0300

    Added method absolute url to models

--- order/models.py
+++ order/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.core.validators import MinValueValidator, DecimalValidator
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -14,6 +15,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('order:product_list_by_category', args=[self.slug])
 
 
 class Product(models.Model):
@@ -48,3 +52,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('order:product_detail', args=[self.id, self.slug])
