b0c870f5d10c1673aae7a229a310e3b635777dbf
Maksim <106922768+Kalininmladshiy@users.noreply.github.com>
2023-02-21 11:14:24 +0300

    Added cart app

--- shop/settings.py
+++ shop/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'order'
+    'order',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                'cart.context_processors.cart',
             ],
         },
     },
@@ -102,3 +105,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CART_SESSION_ID = 'cart'
--- shop/urls.py
+++ shop/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cart/', include(('cart.urls', 'cart'), namespace='cart')),
     path('', include(('order.urls', 'order'), namespace='order')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
